85dacb9759a8d36c043eeb5532798cd3e3775e11
ty221 <dwgipk@gmail.com>
2015-11-27 22:53:02 +0100

    Privacy policy

---
+++ privacy.php
@@ -0,0 +1,83 @@
+<?php
+
+include_once('functions.inc');
+showHead("Polityka prywatności");
+
+?>
+
+<div class="block grayBlock">
+<div class="blockFoldHold"><div class="blockFold"></div><div class="blockFoldClear"></div></div>
+<div class="blockContent">
+<h2>Polityka prywatności</h2>
+<p>1. Informacje ogólne</p>
+<ol>
+<li>Operatorem Serwisu www.ambasadorzy.siecobywatelska.pl jest
+Sieć Obywatelska Watchdog Polska</br>
+ul. Ursynowska 22/2</br>
+02-605 Warszawa</li>
+<li>Serwis realizuje funkcje pozyskiwania informacji o użytkownikach i ich zachowaniach w następujący sposób:
+<ol>
+<li>Poprzez dobrowolnie wprowadzone w formularzach informacje.</li>
+<li>Poprzez zapisywane w urządzeniach końcowych pliki cookie (tzw. „ciasteczka”).</li>
+</ol>
+</li>
+</ol>
+<p>2. Informacje w formularzach</p>
+<ol>
+<li>Serwis zbiera informacje podane dobrowolnie przez użytkownika.</li>
+<li>Serwis może zapisać ponadto informacje o parametrach połączenia (oznaczenie czasu, adres IP)</li>
+<li>Dane dotyczące imienia, nazwiska i miejscowości zamieszkania zadeklarowane przez osoby podpisującej „Apel” są podawane do publicznej wiadomości i przekazywane przewodniczącym partii politycznych.</li>
+<li>Dane podane w formularzu stanowią zbiór, który poszerzy bazę danych prowadzoną przez Operatora i zgłoszoną do Generalnego Inspektora Ochrony Danych Osobowych.</li>
+<li>Dane podane w formularzu są przetwarzane w celu wynikającym z funkcji konkretnego formularza.</li>
+</ol>
+<p>3. Informacja o plikach cookies</p>
+<ol>
+<li>Serwis korzysta z plików cookies.</li>
+<li>Pliki cookies (tzw. „ciasteczka”) stanowią dane informatyczne, w szczególności pliki tekstowe, które przechowywane są w urządzeniu końcowym Użytkownika Serwisu i przeznaczone są do korzystania ze stron internetowych Serwisu. Cookies zazwyczaj zawierają nazwę strony internetowej, z której pochodzą, czas przechowywania ich na urządzeniu końcowym oraz unikalny numer.</li>
+<li>Podmiotem zamieszczającym na urządzeniu końcowym Użytkownika Serwisu pliki cookies oraz uzyskującym do nich dostęp jest operator Serwisu.</li>
+<li><a name="_GoBack"></a> Pliki cookies wykorzystywane są w celu utrzymania sesji Użytkownika Serwisu ze względu na środki bezpieczeństwa i kontrolę uprawnień.</li>
+<li>W ramach Serwisu stosowane są pliki cookies: „sesyjne” (session cookies), czyli tymczasowe, które przechowywane są w urządzeniu końcowym Użytkownika do czasu wylogowania, opuszczenia strony internetowej lub wyłączenia oprogramowania (przeglądarki internetowej).</li>
+<li>Oprogramowanie do przeglądania stron internetowych (przeglądarka internetowa) zazwyczaj domyślnie dopuszcza przechowywanie plików cookies w urządzeniu końcowym Użytkownika. Użytkownicy Serwisu mogą dokonać zmiany ustawień w tym zakresie. Przeglądarka internetowa umożliwia usunięcie plików cookies. Możliwe jest także automatyczne blokowanie plików cookies Szczegółowe informacje na ten temat zawiera pomoc lub dokumentacja przeglądarki internetowej.</li>
+<li>Ograniczenia stosowania plików cookies mogą wpłynąć na niektóre funkcjonalności dostępne na stronach internetowych Serwisu.</li>
+</ol>
+<p>4. Logi serwera</p>
+<ol>
+<li>Informacje o niektórych zachowaniach użytkowników podlegają logowaniu w warstwie serwerowej. Dane te są wykorzystywane wyłącznie w celu administrowania serwisem oraz w celu zapewnienia jak najbardziej sprawnej obsługi świadczonych usług hostingowych.</li>
+<li>Przeglądane zasoby identyfikowane są poprzez adresy URL. Ponadto zapisowi mogą podlegać:
+<ol type="a">
+<li>czas nadejścia zapytania,</li>
+<li>czas wysłania odpowiedzi,</li>
+<li>nazwę stacji klienta – identyfikacja realizowana przez protokół HTTP,</li>
+<li>informacje o błędach jakie nastąpiły przy realizacji transakcji HTTP,</li>
+<li>adres URL strony poprzednio odwiedzanej przez użytkownika (referer link) – w przypadku gdy przejście do Serwisu nastąpiło przez odnośnik,</li>
+<li>informacje o przeglądarce użytkownika,</li>
+<li>Informacje o adresie IP.</li>
+</ol>
+</li>
+<li>Dane powyższe nie są kojarzone z konkretnymi osobami przeglądającymi strony.</li>
+<li>Dane powyższe są wykorzystywane jedynie dla celów administrowania serwerem.</li>
+<li>W przypadku stwierdzenia niestabilnej lub niepoprawnej aplikacji mogą być sporządzane na cele diagnostyczne podręczne dzienniki gromadzące dane, o którym mowa w punk 4 podpunkt 2, a ponadto dane w zakresie:
+<ol type="a">
+<li>dane o pełnym stanie aplikacji w chwili awarii,</li>
+<li>dane umożliwiające powiązanie połączenia z konkretnymi osobami.</li>
+</ol>
+</li>
+</ol>
+<p>5. Udostępnienie danych</p>
+<ol>
+<li>Dane podlegają udostępnieniu podmiotom zewnętrznym wyłącznie w granicach prawnie dozwolonych.</li>
+<li>Dane umożliwiające identyfikację osoby fizycznej są udostępniane wyłączenie za zgodą tej osoby.</li>
+<li>Operator może mieć obowiązek udzielania informacji zebranych przez Serwis upoważnionym organom na podstawie zgodnych z prawem żądań w zakresie wynikającym z żądania.</li>
+</ol>
+<p>6. Zarządzanie plikami cookies – jak w praktyce wyrażać i cofać zgodę?</p>
+<ol>
+<li>Jeśli użytkownik nie chce otrzymywać plików cookies, może zmienić ustawienia przeglądarki. Zastrzegamy, że wyłączenie obsługi plików cookies niezbędnych dla procesów uwierzytelniania, bezpieczeństwa, utrzymania preferencji użytkownika może utrudnić, a w skrajnych przypadkach może uniemożliwić korzystanie poszczególnych elementów aplikacji internetowej.</li>
+</ol>
+</div>
+</div>
+
+<?php
+
+showFooter();
+
+?>
