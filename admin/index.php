45badbf509c74b5f1afc8308ee42bcb40333c76c
ty221 <dwgipk@gmail.com>
2015-11-27 22:25:09 +0100

    Basics of admin panel; default user creation; INC files protection

---
+++ admin/index.php
@@ -0,0 +1,44 @@
+<?php
+
+include_once("functions.inc");
+
+if(!isset($_SESSION['admin'])) header('Location: login.php');
+
+?>
+
+<head>
+<title>Home - adminpanel</title>
+<meta name="robots" content="nofollow, noindex">
+</head>
+<body style="text-align: center; font-size:1.1em; width: 80%; margin: 0 auto; margin-top: 10px">
+<h2>Witaj, <?php echo $_SESSION['admin_name']; ?>!</h2>
+<hr/>
+<?php
+
+$link = mysqli_connect(DB_HOST,DB_USER,DB_PASS,DB_BASE);
+$ambasadorzy = mysqli_query($link, "SELECT * from ambassadors");
+$ambasadorzy_new = mysqli_query($link, "SELECT * from ambassadors WHERE odrzucony = 0 AND zaakceptowany = 0");
+
+
+echo "Aktualna liczba ambasadorów: ".$ambasadorzy->num_rows;
+echo "<br/><i><a href=lista.php>(lista i dane)</a></i><br/>";
+
+if($ambasadorzy_new->num_rows > 0) echo "<br/><b>Oczekujących na weryfikację:</b> ".$ambasadorzy_new->num_rows."<br/><i><a href=waiting.php>(lista i dane)</a></i>";
+echo "<hr/><br/>";
+
+echo "
+
+<a href=login.php>Wyloguj się</a><br/>
+<a href=new.php>Nowy użytkownik</a><br/>
+<a href=newpass.php>Zmień hasło</a><br/>
+<a href=newdata.php>Zmień dane osobowe</a><br/>
+
+<br/>
+<hr/>
+<img src=\"../img/watchdog.png\" alt=\"Logo Watchdog\" /><br/><br/>
+";
+
+
+?>
+
+</body>
---
+++ admin/login.php
@@ -0,0 +1,50 @@
+<?php
+
+include_once("functions.inc");
+
+$failed = 0;
+
+if(isset($_SESSION['admin'])) unset($_SESSION['admin']);
+
+if(isset($_POST['sent']) && csrf_validate($_POST['csrf'])) {
+	$login = $_POST['user'];
+	$pass = $_POST['pass'];
+	
+	$link = mysqli_connect(DB_HOST,DB_USER,DB_PASS,DB_BASE);
+	$login = mysqli_real_escape_string($link, $login);
+	$pass = hash("sha512", $pass);
+	$pass = mysqli_real_escape_string($link, $pass);
+	
+	$user = mysqli_query($link, "SELECT * FROM users WHERE login = '$login' AND password = '$pass' LIMIT 1");
+	if($user->num_rows == 0) {
+		$failed = 1;	
+	}
+	else {
+		$user = mysqli_fetch_row($user);
+		$_SESSION['admin'] = 1;
+		$_SESSION['admin_name'] = $user[3];
+		header('Location: index.php');
+	}
+
+}
+
+?>
+<head>
+<title>Logowanie - adminpanel</title>
+<meta name="robots" content="nofollow, noindex">
+</head>
+<body style="text-align: center">
+<h2>Podaj dane logowania, aby uzyskać dostęp!</h2>
+<?php if($failed) echo "<h3 style='color: red'>Podano nieprawidłowe dane logowania!</h3>"; ?>
+<div style="width: 60%; margin: 0 auto; font-size: 1.2em"><hr />
+<form action=login.php method=post>
+<input type=hidden name=csrf value="<?php echo $_SESSION['csrf']; ?>">
+User: <input type=text name=user required /><br/><br/>
+Hasło: <input type=password name=pass required /><br/><br/>
+<input type=submit value="Zaloguj się do Systemu!" name=sent />
+<hr/>
+<img src="../img/watchdog.png" alt="Logo Watchdog" /><br/><br/>
+<span style="font-size: 0.6em">System wewnętrzny Sieci Obywatelskiej Watchdog Polska. <br/>Próby uzyskania dostępu przez osoby nieuprawnione są zabronione.</span>
+</form>
+</div>
+</body>
