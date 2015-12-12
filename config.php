c9df7a05cb3f161aa7faf74d10e6cf48eb263ea3
ty221 <dwgipk@gmail.com>
2015-12-12 23:55:45 +0100

    Fixed #17; new table for organizations and companies

--- config.php
+++ config.php
@@ -13,8 +13,9 @@ if(isset($_POST['sent'])) {
 		echo "<span style='color: red'>Nie można połączyć się z bazą danych! (Can't connect to DB)</span><br/>";
 	}
 	else {
-		mysqli_query($link,"CREATE TABLE IF NOT EXISTS `users` (`idusers` INT NOT NULL AUTO_INCREMENT,`login` VARCHAR(45) NOT NULL,`password` VARCHAR(128) NOT NULL,`name` VARCHAR(60) NOT NULL, `usuniety` TINYINT(1) NOT NULL DEFAULT 0, PRIMARY KEY (`idusers`)) ENGINE = InnoDB;");
+		mysqli_query($link,"CREATE TABLE IF NOT EXISTS `users` (`idusers` INT NOT NULL AUTO_INCREMENT,`login` VARCHAR(45) NOT NULL,`password` VARCHAR(128) NOT NULL,`name` VARCHAR(60) NOT NULL, `usuniety` TINYINT(1) NOT NULL DEFAULT 0, `lastchange` DATE NOT NULL, PRIMARY KEY (`idusers`)) ENGINE = InnoDB;");
 		mysqli_query($link, "CREATE TABLE IF NOT EXISTS `ambassadors` (`idambassadors` INT NOT NULL AUTO_INCREMENT,`imie` VARCHAR(45) NOT NULL,`nazwisko` VARCHAR(45) NOT NULL,`email` VARCHAR(60) NOT NULL,`miasto` VARCHAR(45) NOT NULL,`telefon` VARCHAR(15) NOT NULL,`zawod` VARCHAR(200) NOT NULL,`dlaczego` TEXT NOT NULL DEFAULT '',`adres` VARCHAR(200) NULL,`zdjecie` TEXT NULL,`zaakceptowany` TINYINT(1) NOT NULL DEFAULT 0,`odrzucony` TINYINT(1) NOT NULL,`data` TIMESTAMP NOT NULL DEFAULT NOW(),`ip` TEXT NOT NULL, PRIMARY KEY (`idambassadors`)) ENGINE = InnoDB;");
+		mysqli_query($link, "CREATE TABLE IF NOT EXISTS `orgs` (`idorgs` INT NOT NULL AUTO_INCREMENT, `nazwa` VARCHAR(128) NOT NULL, `miasto` VARCHAR(45) NOT NULL, `telefon` VARCHAR(15) NOT NULL, `email` VARCHAR(60) NOT NULL, `specjalizacja` TEXT NOT NULL, `strona` VARCHAR(60) NULL, `adres` VARCHAR(200) NOT NULL, `logo` VARCHAR(100) NULL, PRIMARY KEY (`idorgs`)) ENGINE = InnoDB");		
 		
 		mysqli_query($link,"INSERT INTO `users` (`idusers`, `login`, `password`, `name`) VALUES (NULL, 'root', 'ba3253876aed6bc22d4a6ff53d8406c6ad864195ed144ab5c87621b6c233b548baeae6956df346ec8c17f5ea10f35ee3cbc514797ed7ddd3145464e2a0bab413', 'Administrator');");
 		
