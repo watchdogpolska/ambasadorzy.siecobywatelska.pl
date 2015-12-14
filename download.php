93a77f5d389e46ed4d436cf4475e514dcafa4933
ty221 <dwgipk@gmail.com>
2015-12-14 17:12:41 +0100

    Added new promotional materials to download page

--- download.php
+++ download.php
@@ -15,6 +15,9 @@ showHead("Do pobrania");
 <li><a target="_blank" href="files/kodeks.pdf">* Kodeks Ambasadorów Jawności (PDF)</a></li>
 <li><a target="_blank" href="files/dyspozycje.pdf">* Przykładowe dyspozycje (PDF)</a></li>
 <li><a target="_blank" href="files/podpisy.pdf">* Podpisy do zdjęć (PDF)</a></li>
+<li><a target="_blank" href="files/ulotka.pdf">* Ulotka pt. "Zostań Ambasadorem Jawności!" (PDF)</a></li>
+<li><a target="_blank" href="files/ulotka2.pdf">* Ulotka nt. dostępu do informacji publicznej (PDF)</a></li>
+<li><a target="_blank" href="files/pub_zeby.pdf">* Publikacja pt. "Zdrowe zęby demokracji" (PDF)</a></li>
 </ul>
 </div></div>
 
