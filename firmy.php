20f0df721673f61c4523747d4b91cf90f306ed1c
Dzakus <Dzakus13@gmail.com>
2016-01-05 00:53:24 +0100

    Change list style and block color

--- firmy.php
+++ firmy.php
@@ -11,24 +11,24 @@ showHead("Firmy");
 	}
 </style>
 
-<div class="block redBlock" >
+<div class="block grayBlock" >
 	<div class="blockContent">
 		<h2>Dla firm</h2>
 		Do naszego programu Ambasadorów/Ambasadorek Jawności zapraszamy firmy, które chcą wesprzeć ideę państwa otwartego, odpowiedzialnego i przyjaznego obywatelom.
 		<h3>Do jakich firm kierowana jest oferta?</h3>
 		Do udziału w programie zapraszamy Firmy z całej Polski, które:
-		<ul style="list-style: square inside">
+		<ul class="list-dotted-brand">
 			<li>cenią takie wartości jak: przejrzystość, odpowiedzialność i etyczne działanie.</li>
 			<li>Chcą wspierać te wartości i promować je.</li>
 			<li>Są gotowe podpisać Deklarację przejrzystości i jej przestrzegać.</li>
 		</ul>
 		<h3>Warunki współpracy</h3>
-		<ul style="list-style: square inside">
+		<ul class="list-dotted-brand">
 			<li> Kadencja w programie trwa rok. Po tym okresie istnieje możliwość jej przedłużenia na ustalonych wcześniej warunkach. </li>
 			<li> Przystępująca do programu firma zobowiązuje się do przestrzegania deklaracji przejrzystości, promowania idei jawności oraz wsparcia działań Sieci Obywatelskiej Watchdog Polska zadeklarowaną kwotą w ustalonym systemie (jednorazowo, kwartalnie, miesięcznie). </li>
 			<li> Sieć Obywatelska Watchdog Polska zapewnia firmom zaproszonym do współpracy udział w wydarzeniach projawnościowych oraz pakiet promocyjny, m.in. Informacje na temat firmy będą widoczne m.in. na na stronie www.ambasadorzy.siecobywatelska.pl oraz mapie Wspierających jawność, na stronie siecobywatelska.pl w zakładce Program Ambasadorzy Jawności.</li>
 		</ul>
-		Szczegółowe warunki współpracy będą omówione i ustalone z firmami deklarującymi chęć współpracy.
+		<p>Szczegółowe warunki współpracy będą omówione i ustalone z firmami deklarującymi chęć współpracy.</p>
 		<h3>W jaki sposób firma zostaje Ambasadorem Jawności?</h3>
 		<ol>
 			<li>Zgłoszenie/zaproszenie do programu.</li>
@@ -40,14 +40,14 @@ showHead("Firmy");
 		</ol>
 		<h3>W jaki sposób sprawdzamy dużą firmę:</h3>
 		Śledzimy informacje na temat firmy na stronie internetowej, prasie branżowej oraz rankingach branżowych. Sprawdzamy:
-		<ul style="list-style: square inside">
+		<ul class="list-dotted-brand">
 			<li>Czy współpracuje z organizacjami pozarządowymi bądź działa pro bono;</li>
 			<li>Czy prowadzi działania z zakresu CSR;</li>
 			<li>Czy wyznaje wartości zbieżne z naszymi; </li>
 			<li>opinie klientów i pracowników;</li>
 		</ul>
 		<h3>Mniejszą firmę (poniżej 10 prawników) prosimy o odpowiedź na następujące pytania:</h3>
-		<ul style="list-style: square inside">
+		<ul class="list-dotted-brand">
 			<li>Dlaczego chce być Ambasadorem Jawności?</li>
 			<li>Czy i jak działa w zakresie odpowiedzialnego biznesu, czy i jakie organizacje pozarządowe wspiera?</li>
 			<li>Prosimy o dwie referencje od klientów.</li>
