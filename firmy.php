880119399d15ac82925b46e4d3c2a87d951a7a78
Dzakus <Dzakus13@gmail.com>
2016-01-17 13:23:49 +0100

    Corrections to the page Firmy according to the recommendations

--- firmy.php
+++ firmy.php
@@ -4,19 +4,12 @@ include_once('functions.inc.php');
 showHead("Firmy", "Oferta dla biznesu");
 
 ?>
-<style>
-	h3 {
-		text-align: left;
-		margin: 25px 25px;
-	}
-</style>
-
 <div class="block grayBlock" >
 	<div class="blockContent">
 		<h2>Dla firm</h2>
-		Do naszego programu Ambasadorów/Ambasadorek Jawności zapraszamy firmy, które chcą wesprzeć ideę państwa otwartego, odpowiedzialnego i przyjaznego obywatelom.
+		<p>Do naszego programu Ambasadorów/Ambasadorek Jawności zapraszamy firmy, które chcą wesprzeć ideę państwa otwartego, odpowiedzialnego i przyjaznego obywatelom.</p>
 		<h3>Do jakich firm kierowana jest oferta?</h3>
-		Do udziału w programie zapraszamy Firmy z całej Polski, które:
+		<p>Do udziału w programie zapraszamy Firmy z całej Polski, które:</p>
 		<ul class="list-dotted-brand">
 			<li>Cenią takie wartości jak: przejrzystość, odpowiedzialność i etyczne działanie.</li>
 			<li>Chcą wspierać te wartości i promować je.</li>
@@ -24,9 +17,9 @@ showHead("Firmy", "Oferta dla biznesu");
 		</ul>
 		<h3>Warunki współpracy</h3>
 		<ul class="list-dotted-brand">
-			<li> Kadencja w programie trwa rok. Po tym okresie istnieje możliwość jej przedłużenia na ustalonych wcześniej warunkach. </li>
-			<li> Przystępująca do programu firma zobowiązuje się do przestrzegania deklaracji przejrzystości, promowania idei jawności oraz wsparcia działań Sieci Obywatelskiej Watchdog Polska zadeklarowaną kwotą w ustalonym systemie (jednorazowo, kwartalnie, miesięcznie). </li>
-			<li> Sieć Obywatelska Watchdog Polska zapewnia firmom zaproszonym do współpracy udział w wydarzeniach projawnościowych oraz pakiet promocyjny, m.in. Informacje na temat firmy będą widoczne m.in. na na stronie www.ambasadorzy.siecobywatelska.pl oraz mapie Wspierających jawność, na stronie siecobywatelska.pl w zakładce Program Ambasadorzy Jawności.</li>
+			<li>Kadencja w programie trwa rok. Po tym okresie istnieje możliwość jej przedłużenia na ustalonych wcześniej warunkach. </li>
+			<li>Przystępująca do programu firma zobowiązuje się do przestrzegania deklaracji przejrzystości, promowania idei jawności oraz wsparcia działań Sieci Obywatelskiej Watchdog Polska zadeklarowaną kwotą w ustalonym systemie (jednorazowo, kwartalnie, miesięcznie). </li>
+			<li>Sieć Obywatelska Watchdog Polska zapewnia firmom zaproszonym do współpracy udział w wydarzeniach projawnościowych oraz pakiet promocyjny, m.in. informacje na temat firmy będą widoczne na stronie www.ambasadorzy.siecobywatelska.pl oraz mapie Wspierających jawność, na stronie siecobywatelska.pl w zakładce Program Ambasadorzy Jawności.</li>
 		</ul>
 		<p>Szczegółowe warunki współpracy będą omówione i ustalone z firmami deklarującymi chęć współpracy.</p>
 		<h3>W jaki sposób firma zostaje Ambasadorem Jawności?</h3>
@@ -38,21 +31,21 @@ showHead("Firmy", "Oferta dla biznesu");
 			<li>Podpisanie Deklaracji przejrzystości.</li>
 			<li>Realizacja działań w zakresie Programu Ambasadorzy Jawności.</li>
 		</ol>
-		<h3>W jaki sposób sprawdzamy dużą firmę?</h3>
-		Śledzimy informacje na temat firmy na stronie internetowej, prasie branżowej oraz rankingach branżowych. Sprawdzamy:
+		<h3>W jaki sposób sprawdzamy dużą firmę</h3>
+		<p>Śledzimy informacje na temat firmy na stronie internetowej, prasie branżowej oraz rankingach branżowych. Sprawdzamy:</p>
 		<ul class="list-dotted-brand">
 			<li>Czy współpracuje z organizacjami pozarządowymi bądź działa pro bono?</li>
 			<li>Czy prowadzi działania z zakresu CSR?</li>
-			<li>Czy wyznaje wartości zbieżne z naszymi?</li>
-			<li>Opinie klientów i pracowników.</li>
+			<li>Czy wyznaje wartości zbieżne z naszymi? </li>
+			<li>opinie klientów i pracowników;</li>
 		</ul>
 		<h3>Mniejszą firmę (poniżej 10 pracowników) prosimy o odpowiedź na następujące pytania:</h3>
 		<ul class="list-dotted-brand">
 			<li>Dlaczego chce być Ambasadorem Jawności?</li>
 			<li>Czy i jak działa w zakresie odpowiedzialnego biznesu, czy i jakie organizacje pozarządowe wspiera?</li>
-			<li>Dwie referencje od klientów.</li>
+			<li>O dwie referencje od klientów.</li>
 		</ul>
-		Zainteresowane firmy prosimy o <a href="contact.php">kontakt telefoniczny lub mailowy.</a>
+		<p>Zainteresowane firmy prosimy o <a href="contact.php">kontakt telefoniczny lub mailowy</a>.</p>
 	</div>
 </div>
 
