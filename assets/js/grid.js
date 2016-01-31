671f8662d9d7424474cfdd3ed63ac81b6ffcaab2
Dzakus <Dzakus13@gmail.com>
2016-01-31 12:48:44 +0100

    Add grid for with-us

--- assets/js/grid.js
+++ assets/js/grid.js
@@ -1,6 +1,33 @@
-$('.grid').masonry({
-  itemSelector: '.grid-item',
-  percentPosition: true,
-  columnWidth: '.grid-sizer',
-  // gutter: '.grid-gutter'
-});
+(function (){
+	$(function() {
+		var $grid = $('.grid').masonry({
+			itemSelector: '.grid-item',
+			percentPosition: true,
+			columnWidth: '.grid-sizer',
+			stamp: '.grid-stamp'
+		}).imagesLoaded( function() {
+			// init Masonry after all images have loaded
+			$grid.masonry();
+		});;
+
+		$('.js-expand-card').click(function (e) {
+			e.preventDefault();
+			var $card = $(this).closest('.card2');
+			var $grid_item = $card.closest('.grid-item');
+
+			$card.toggleClass('expanded');
+			$grid_item.toggleClass('expanded');
+
+			$(this).text($card.hasClass('expanded') ? "Zwiń <" : "Rozwiń >");
+			// Refresh grid;
+			$grid.masonry();
+		});
+
+		// Wait for iframe ex. Youtube
+		// setTimeout(function() {
+		// 	// Refresh grid;
+		// 	$grid.masonry();
+		// }, 500)
+
+	});
+} (jQuery));
--- withus.php
+++ withus.php
@@ -11,7 +11,18 @@ showHead("Są z nami", "&nbsp;");
 
 
 <div class="row">
-	<div class="col-md-6">
+	<div class="grid" id="with-us-grid">
+		<div class="grid-sizer grid-item-with-us"></div>
+		<div class="grid-stamp grid-size-xs-1-1 grid-size-md-1-2 grid-size-stamp-top-right">
+			<div class="card">
+				<div class="card--content">
+					<div class="embed-responsive embed-responsive-16by9">
+						<iframe class="embed-responsive-item" src="https://www.youtube.com/embed/5feZrqYOisY" frameborder="0" allowfullscreen></iframe>
+					</div>
+				</div>
+			</div>
+		</div>
+
 		<?php
 		$link = mysqli_connect(DB_HOST,DB_USER,DB_PASS,DB_BASE);
 		$celebryci = mysqli_query($link, "SELECT * from famous");
@@ -22,39 +33,48 @@ showHead("Są z nami", "&nbsp;");
 			$desc = htmlspecialchars($celebryta['desc']);
 
 			?>
-			<div id="descriptionBlock" class="block grayBlock" >
-				<div class="blockContent">
-					<h2><?php echo $name; ?></h2>
-					<?php
-					if (strpos($link,'youtube') !== false) {
-						$search = '/youtube\.com\/watch\?v=([a-zA-Z0-9]+)/smi';
-						$replace = "youtube.com/embed/$1";
-						$url = preg_replace($search,$replace,$link);
-						echo '<iframe class="embed-responsive-item" src="'.$url.'" frameborder="0" allowfullscreen></iframe>';
-					}
-					else echo "<img style='max-height: 200px; display: block; margin: 0 auto' src='$link' alt='$name'>";
-
-					echo "<br/>".$desc;
-					?>
-
+			<div class="grid-item grid-item-with-us">
+				<?php
+				if (strpos($link,'youtube') !== false) {
+					$search = '/youtube\.com\/watch\?v=([a-zA-Z0-9]+)/smi';
+					$replace = "youtube.com/embed/$1";
+					$url = preg_replace($search,$replace,$link);
+					$isYT = true;
+					$yt_embed = '<iframe class="embed-responsive-item" src="'.$url.'" frameborder="0" allowfullscreen></iframe>';
+				}
+				else{
+					$isYT = false;
+				}
+				?>
+				<div class="card2">
+					<div class="card2--summary">
+						<div class="card2--holder">
+							<?php
+							if(!$isYT):
+							?>
+							<img src='<?php echo $link; ?>' alt='<?php echo $name; ?>'>
+							<?php
+							endif;
+							?>
+						</div>
+						<?php
+						if($isYT){
+							echo $yt_embed;
+						}
+						?>
+						<div class="card2--short">
+							<h2><?php echo $name; ?></h2>
+							<a href="#" class="js-expand-card">Rozwiń ></a>
+						</div>
+					</div>
+					<div class="card2--detail">
+						<?php echo $desc; ?>
+					</div>
 				</div>
-
 			</div>
 			<?php
 		}
 		?>
-	</div>
-
-
-
-	<div class="col-md-6">
-		<div id="signFormBlock" class="block redBlock">
-			<div class="blockContent">
-				<div class="embed-responsive embed-responsive-16by9">
-					<iframe class="embed-responsive-item" src="https://www.youtube.com/embed/5feZrqYOisY" frameborder="0" allowfullscreen></iframe>
-				</div>
-			</div>
-		</div>
 	</div>
 </div>
 <?php
