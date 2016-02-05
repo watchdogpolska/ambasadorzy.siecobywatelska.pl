926955aee105810637204b9fcd94ffdfab3d9378
Dzakus <Dzakus13@gmail.com>
2016-02-05 17:47:34 +0100

    Add style for map

--- ambassadors.php
+++ ambassadors.php
@@ -63,10 +63,182 @@ $amb = mysqli_query($link, "SELECT * FROM ambassadors WHERE zaakceptowany = 1");
 
 	function initMap() {
 		var latlng = new google.maps.LatLng(52.25, 18.66);
+		var style = [{
+			"featureType": "administrative", "elementType": "geometry.fill",
+			"stylers": [{
+				"visibility": "off"
+			}]
+		}, {
+			"featureType": "administrative",
+			"elementType": "labels.text",
+			"stylers": [{
+				"visibility": "on"
+			}, {
+				"color": "#8e8e8e"
+			}]
+		}, {
+			"featureType": "administrative",
+			"elementType": "labels.text.fill",
+			"stylers": [{
+				"color": "#7f7f7f"
+			}]
+		}, {
+			"featureType": "administrative",
+			"elementType": "labels.text.stroke",
+			"stylers": [{
+				"visibility": "off"
+			}]
+		}, {
+			"featureType": "administrative.country",
+			"elementType": "geometry.stroke",
+			"stylers": [{
+				"color": "#bebebe"
+			}]
+		}, {
+			"featureType": "administrative.province",
+			"elementType": "geometry.stroke",
+			"stylers": [{
+				"visibility": "on"
+			}, {
+				"color": "#cbcbcb"
+			}, {
+				"weight": "0.69"
+			}]
+		}, {
+			"featureType": "administrative.locality",
+			"elementType": "geometry",
+			"stylers": [{
+				"visibility": "simplified"
+			}]
+		}, {
+			"featureType": "landscape",
+			"elementType": "all",
+			"stylers": [{
+				"color": "#e4e4e4"
+			}]
+		}, {
+			"featureType": "poi",
+			"elementType": "all",
+			"stylers": [{
+				"visibility": "off"
+			}]
+		}, {
+			"featureType": "road",
+			"elementType": "all",
+			"stylers": [{
+				"saturation": -100
+			}, {
+				"lightness": 45
+			}, {
+				"visibility": "simplified"
+			}]
+		}, {
+			"featureType": "road",
+			"elementType": "geometry.stroke",
+			"stylers": [{
+				"visibility": "off"
+			}]
+		}, {
+			"featureType": "road",
+			"elementType": "labels",
+			"stylers": [{
+				"visibility": "off"
+			}]
+		}, {
+			"featureType": "road.highway",
+			"elementType": "all",
+			"stylers": [{
+				"visibility": "simplified"
+			}, {
+				"color": "#dadada"
+			}]
+		}, {
+			"featureType": "road.highway",
+			"elementType": "labels",
+			"stylers": [{
+				"visibility": "off"
+			}]
+		}, {
+			"featureType": "road.highway",
+			"elementType": "labels.text",
+			"stylers": [{
+				"visibility": "simplified"
+			}]
+		}, {
+			"featureType": "road.arterial",
+			"elementType": "all",
+			"stylers": [{
+				"visibility": "on"
+			}]
+		}, {
+			"featureType": "road.arterial",
+			"elementType": "labels.text",
+			"stylers": [{
+				"visibility": "simplified"
+			}]
+		}, {
+			"featureType": "road.arterial",
+			"elementType": "labels.icon",
+			"stylers": [{
+				"visibility": "off"
+			}]
+		}, {
+			"featureType": "road.local",
+			"elementType": "all",
+			"stylers": [{
+				"visibility": "simplified"
+			}]
+		}, {
+			"featureType": "road.local",
+			"elementType": "geometry",
+			"stylers": [{
+				"color": "#eeeeee"
+			}]
+		}, {
+			"featureType": "road.local",
+			"elementType": "labels.text",
+			"stylers": [{
+				"visibility": "simplified"
+			}]
+		}, {
+			"featureType": "transit",
+			"elementType": "all",
+			"stylers": [{
+				"visibility": "off"
+			}]
+		}, {
+			"featureType": "water",
+			"elementType": "all",
+			"stylers": [{
+				"color": "#cbcbcb"
+			}, {
+				"visibility": "on"
+			}]
+		}, {
+			"featureType": "water",
+			"elementType": "geometry.fill",
+			"stylers": [{
+				"color": "#d9d9d9"
+			}]
+		}, {
+			"featureType": "water",
+			"elementType": "geometry.stroke",
+			"stylers": [{
+				"visibility": "off"
+			}]
+		}, {
+			"featureType": "water",
+			"elementType": "labels.text",
+			"stylers": [{
+				"visibility": "simplified"
+			}]
+		}];
 		var mapOptions = {
 			zoom: 5,
-			center: latlng
-		}
+			center: latlng,
+			styles: style
+		};
+
 		map = new google.maps.Map(document.getElementById("map"), mapOptions);
 		geocoder = new google.maps.Geocoder();
 	}
